ba8ac280c3252ac534949beac13f5bccd41f2811
Alex Wolf <f.alexander.wolf@gmail.com>
2025-11-13 21:07:05 +0100

    🔖 Release 0.2.0

--- publishprs/__init__.py
+++ publishprs/__init__.py
@@ -1,5 +1,5 @@
 """Publish private PRs."""
 
-__version__ = "0.1.0"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.2.0"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 from .core import Publisher
