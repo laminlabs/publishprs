3accf6c4e900ae377c6b4b154c28a7fdda287547
Alex Wolf <f.alexander.wolf@gmail.com>
2025-11-16 20:12:02 +0100

    ♻️ Attempt test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,7 @@
 """Test publishprs functionality."""
 
+import os
+
 from publishprs import Publisher
 
 
@@ -9,13 +11,15 @@ def test_publish_pr():
     This test uses PR #3820 from the private publishprs repo which contains
     user-attached images that need to be processed and uploaded to LaminDB.
     """
+    target_token = os.getenv("GH_LAMINHUB_PUBLIC_SUNNYOSUN")
     publisher = Publisher(
         source_repo="https://github.com/laminlabs/publishprs",
         target_repo="https://github.com/laminlabs/laminhub-public",
+        target_token=target_token,
     )
     url = publisher.publish(
-        pull_id=1,
-        close_pr=False,
+        pull_id=4,
+        close_pr=True,
     )
     assert url.startswith("https://github.com/laminlabs/laminhub-public/pull/")
     assert url.split("/")[-1].isdigit()  # PR number should be numeric
